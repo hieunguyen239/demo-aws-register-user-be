b8eef23e87859b58caa560ae0fa1f678f0bd233e
Hieu Nguyen <hieu.nguyenminh88@gmail.com>
2023-05-05 16:02:35 +0700

    test s3 create

--- src/functions/registerUser.js
+++ src/functions/registerUser.js
@@ -1,5 +1,6 @@
 const formParser = require('lambda-multipart-parser');
-const {makeResponse} = require('../helpers/makeResponse');
+const { makeResponse } = require('../helpers/makeResponse');
+const AWS = require('aws-sdk');
 
 module.exports.handler = async (event) => {
   const formData = await formParser.parse(event);
@@ -15,8 +16,29 @@ module.exports.handler = async (event) => {
       message: 'Picture, name, dob required',
     });
   }
+  try {
+    const s3 = new AWS.S3();
+    await s3.putObject({
+      Bucket: 'local-bucket',
+      Key: 'test.txt',
+      Body: new Buffer("abcd")
+    }).promise;
 
-  return makeResponse({
-    statusCode: 200,
-  });
+    return makeResponse({
+      statusCode: 200,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return makeResponse({
+      statusCode: 500,
+      message: error,
+    });
+  }
+
+  // console.log(2222222);
+
+  // return makeResponse({
+  //   statusCode: 200,
+  // });
 };
