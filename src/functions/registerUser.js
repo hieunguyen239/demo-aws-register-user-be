78573e640da37a534221190028a1dbbcacb42e83
Hieu Nguyen <hieu.nguyenminh88@gmail.com>
2023-06-04 15:18:29 +0700

    - allow s3 bucket acl public read

--- src/functions/registerUser.js
+++ src/functions/registerUser.js
@@ -2,12 +2,17 @@ const formParser = require('lambda-multipart-parser');
 const { makeResponse } = require('../helpers/makeResponse');
 const AWS = require('aws-sdk');
 
+const BUCKET_NAME = process.env.APP_S3BUCKET_NAME;
+const DYNAMODB_TABLE = process.env.APP_DYNAMODB_TABLE;
+
 module.exports.handler = async (event) => {
   const formData = await formParser.parse(event);
   const {
     files,
     name,
     dob,
+    role,
+    email,
   } = formData || {};
 
   if (!files.length || !name || !dob) {
@@ -17,14 +22,26 @@ module.exports.handler = async (event) => {
     });
   }
 
+  console.log('Validate data success');
+
   try {
     const s3 = new AWS.S3();
+    const dynamoDB = new AWS.DynamoDB();
 
     await s3.putObject({
-      Bucket: 'demo-register-user-bucket',
+      Bucket: BUCKET_NAME,
       Key: files[0].filename,
-      Body:files[0].content,
-      // ACL:'public-read',
+      Body: files[0].content,
+      ACL:'public-read',
+    }).promise();
+
+    await dynamoDB.putItem({
+      TableName: DYNAMODB_TABLE,
+      Item: {
+        email: { S: email },
+        role: { S: role },
+        userData: { S: JSON.stringify({ name, dob, avatar: files[0].filename }) },
+      }
     }).promise();
 
     return makeResponse({
